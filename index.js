b95c7b65681b34e1c42af9521a477a6995a2ad5a
chintan.dave <chintan.dave@spec-india.com>
2021-03-08 19:18:43 +0530

    Changes of 03082021

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 //https://www.youtube.com/watch?v=oykl1Ih9pMg  -- Deployment f NodeJs application in ubuntu OS
 // How to define acess control policy in node js server
 //https://rapidapi.com/blog/react-api-authentication-authorization/
@@ -7,54 +6,74 @@
 //https://medium.com/m/global-identity?redirectUrl=https%3A%2F%2Flevelup.gitconnected.com%2Fhow-to-kill-server-when-seeing-eaddrinuse-address-already-in-use-16c4c4d7fe5d
 //https://developers.google.com/gmail/api/quickstart/nodejs
 
-const express = require('express')
-const jwt = require('jsonwebtoken')
+const express = require("express");
+const jwt = require("jsonwebtoken");
+//var bodyParser = require("body-parser");
 const app = express();
-var cors = require('cors')
+const config = require("./config.json");
+var cors = require("cors");
 const port = 8000;
+const tokenList = {};
 
-app.use(cors())
+app.use(cors());
+app.use(express.json());
 
-app.get('/loginStatus1', (req, res) => {
-    res.send(
-      {'loginStatus':true}
-    );
+app.get("/", (req, res) => {
+  res.send({ loginStatus: true });
 });
 
-app.get('/loginStatus2', (req, res) => {
-    res.send(
-        {'loginStatus':false}
-        );
+app.get("/loginStatus2", (req, res) => {
+  res.send({ loginStatus: false });
 });
-app.get('/', (req, res) => {
-    var sql = require("mssql");
-    // config for your database
-    var config = {
-        user: 'sa3',
-        password: 'mypc98247#',
-        server: 'localhost', 
-        database: 'Sample' 
-    };
 
-    // connect to your database
-    sql.connect(config, function (err) {
-    
-        if (err) console.log(err);
+app.post("/login", (req, res) => {
+  const postData = req.body;
 
-        // create Request object
-        var request = new sql.Request();
-           
-        // query to the database and get the records
-        request.query('SELECT * from Tbl_Employee', function (err, recordset) {
-            
-            if (err) console.log(err);
-            // send records as a response
-            res.send(recordset);
-            
-        });
+  const user = {
+    email: req.body.email,
+    name: req.body.name,
+  };
+
+  const token = jwt.sign(user, config.secret, { expiresIn: config.tokenLife });
+  const refreshtoken = jwt.sign(user, config.refreshTokenSecret, {
+    expiresIn: config.refreshTokenLife,
+  });
+
+  const response = {
+    status: "Logged in",
+    token: token,
+    refreshToken: refreshtoken,
+  };
+  tokenList[refreshtoken] = response;
+  res.status(200).json(response);
+});
+
+app.get("/SQLConnection", (req, res) => {
+  var sql = require("mssql");
+  // config for your database
+  var config = {
+    user: "sa3",
+    password: "mypc98247#",
+    server: "localhost",
+    database: "Sample",
+  };
+
+  // connect to your database
+  sql.connect(config, function (err) {
+    if (err) console.log(err);
+
+    // create Request object
+    var request = new sql.Request();
+
+    // query to the database and get the records
+    request.query("SELECT * from Tbl_Employee", function (err, recordset) {
+      if (err) console.log(err);
+      // send records as a response
+      res.send(recordset);
     });
+  });
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`)
+  console.log(`Example app listening on port ${port}!`);
 });
